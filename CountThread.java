06b6cd2b0a262bf07ac4882150ead9aa2dc12139
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-08-16 09:39:40 -0500

    Imprime por pantalla un rango de numeros

---
+++ CountThread.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hcadavid
+ */
+public class CountThread extends Thread {
+    private int initial;
+    private int end;
+
+    public CountThread(int initial, int end) {
+        this.initial = initial;
+        this.end = end;
+    }
+
+
+    public void increment() {
+        for (int i = initial; i <= end; i++) {
+            System.out.println("Counting: " + i);
+        }
+    }
+    
+
+    
+}
