5ac88d4395d45567153260121cbf4a09de56ea19
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-08-18 15:33:59 -0500

    Organización

--- Thread/CountThread.java
+++ Thread/CountThread.java
@@ -1,3 +1,4 @@
+package Thread;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -21,7 +22,7 @@ public class CountThread extends Thread {
     public void run() {
         increment();
     }
-    
+
     public void increment() {
         for (int i = initial; i <= end; i++) {
             System.out.println("Counting: " + i);
